8d7f742cdc16870dbc35f88c5f55c33458097d17
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2025-10-18 16:18:46 -0400

    Fix app tests on win-arm64

--- NAPS2.App.Tests/Targets/WindowsAppTestTarget.cs
+++ NAPS2.App.Tests/Targets/WindowsAppTestTarget.cs
@@ -1,18 +1,23 @@
+using System.Runtime.InteropServices;
+
 namespace NAPS2.App.Tests.Targets;
 
 public class WindowsAppTestTarget : IAppTestTarget
 {
-    public AppTestExe Console => GetAppTestExe("NAPS2.App.Console", "NAPS2.Console.exe", "win-x64");
-    public AppTestExe Gui => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", "win-x64");
+    private static readonly string PlatformArch =
+        RuntimeInformation.ProcessArchitecture == Architecture.Arm64 ? "win-arm64" : "win-x64";
+
+    public AppTestExe Console => GetAppTestExe("NAPS2.App.Console", "NAPS2.Console.exe", PlatformArch);
+    public AppTestExe Gui => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", PlatformArch);
     public AppTestExe Worker => GetAppTestExe("NAPS2.App.Worker", "NAPS2.Worker.exe", "win-x86", null, "lib");
-    public AppTestExe Server => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", "win-x64", "server");
+    public AppTestExe Server => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", PlatformArch, "server");
     public bool IsWindows => true;
 
     private AppTestExe GetAppTestExe(string project, string exeName, string arch, string argPrefix = null,
         string testRootSubPath = null)
     {
         return new AppTestExe(
-            Path.Combine(AppTestHelper.SolutionRoot, project, "bin", "Debug", "net9-windows", arch),
+            Path.Combine(AppTestHelper.SolutionRoot, project, "bin", "Debug", "net10.0-windows", arch),
             exeName,
             argPrefix,
             testRootSubPath);
